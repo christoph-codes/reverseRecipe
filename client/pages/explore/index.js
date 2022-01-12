8bcef937274c6f2bea9ed17e67d3908de5049f06
Christopher Jones <christopher.jones@creditone.com>
2022-01-11 18:49:32 -0800

    75: Add new explore page as an example of how to use the new page template

---
+++ client/pages/explore/index.js
@@ -0,0 +1,15 @@
+import PageTemplate from '../../templates/PageTemplate';
+import styles from './Explore.module.scss';
+
+const Explore = () => {
+	return (
+		<PageTemplate
+			title="Explore Recipes with Reverse Recipe"
+			className={styles.Explore}
+		>
+			Explore
+		</PageTemplate>
+	);
+};
+
+export default Explore;
