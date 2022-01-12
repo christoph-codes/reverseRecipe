8845fad3b364726279cb935df337bc764cdbf8e4
Christopher Jones <tkcwebdev@gmail.com>
2022-01-11 17:28:34 -0800

    42: Add multiple variants to tag component for featured flag for Recipe card

--- client/components/Tag/Tag.jsx
+++ client/components/Tag/Tag.jsx
@@ -1,9 +1,27 @@
 import { string } from 'prop-types';
 import styles from './Tag.module.scss';
 
-const Tag = ({ children, className, ...rest }) => {
+const Tag = ({ children, className, variant, ...rest }) => {
+	const getVariant = () => {
+		switch (variant) {
+			case 'primary':
+				return styles.primary;
+			case 'secondary':
+				return styles.secondary;
+			case 'blue':
+				return styles.blue;
+			case 'green':
+				return styles.green;
+			case 'red':
+				return styles.red;
+			case 'yellow':
+				return styles.yellow;
+			default:
+				return '';
+		}
+	};
 	return (
-		<div className={`${styles.Tag} ${className}`} {...rest}>
+		<div className={`${styles.Tag} ${getVariant()} ${className}`} {...rest}>
 			{children}
 		</div>
 	);
