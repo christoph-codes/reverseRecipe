5b7bb742d7378773731f35ab9418557c7f3d2266
codymolisee <cody.molisee@creditone.com>
2022-04-22 15:30:30 -0700

    ammended realm-cli pull

--- server/functions/http/getAllRecipes.js
+++
@@ -1,6 +0,0 @@
-exports = async function(payload, response) {
-  const request = context.services.get('mongodb-atlas').db('reverseRecipeDB').collection('reverseRecipeRecipe');
-  const data = await request.find({}).toArray();
-  response.setStatusCode(200);
-  response.setBody(JSON.stringify(data));
-}
